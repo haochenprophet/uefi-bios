98231893c8610f73701358a95ec50f255cd615d9
haochenprophet <hao.chen@prophet.net.cn>
2018-01-30 13:45:04 +0800

    Add struct SsmbiosEntry32 SsmbiosEntry64 SsmbiosHeader

--- cPlusPkg/smbios_type.h
+++ cPlusPkg/smbios_type.h
@@ -12,12 +12,50 @@ namespace n_smbios_type
 	};
 	
 
+	typedef struct StructSmbiosEntry32
+	{
+		UINT8  AnchorString[4];//_SM_
+		UINT8  EntryPointStructureChecksum;
+		UINT8  EntryPointLength;
+		UINT8  SMBIOSMajorVersion;
+		UINT8  SMBIOSMinorVersion;
+		UINT16 MaximumStructureSize;
+		UINT8  EntryPointRevision;
+		UINT8  FormattedArea[5];
+		UINT8  IntermediateAnchorString[5];//_DMI_
+		UINT8  IntermediateChecksum;
+		UINT16 StructureTableLength;
+		UINT32 StructureTableAddress;
+		UINT16 NumberOfSMBIOSStructures;
+		UINT8  SMBIOSBCDRevision;
+	}SsmbiosEntry32;
+
+	typedef struct StructSmbiosEntry64
+	{
+		UINT8   AnchorString[5];//_SM3_
+		UINT8   EntryPointStructureChecksum;
+		UINT8   EntryPointLength;
+		UINT8   SMBIOSMajorVersion;
+		UINT8   SMBIOSMinorVersion;
+		UINT8   SMBIOSDocrev;
+		UINT8   EntryPointRevision;
+		UINT8   Reserved;
+		UINT32  StructureTableMaximumSize;//_DMI_
+		UINT64  StructureTableAddress;
+	}SsmbiosEntry64;
+
+	typedef struct StructSmbiosHeader
+	{
+		UINT8  Type;//_SM_
+		UINT8  Length;
+		UINT16 Handle;
+	}SsmbiosHeader;
+
 	class  CsmbiosEntry
 	{
 	public:
-		UINT32 AnchorString;//_SM_
-		UINT8  EntryPointStructureChecksum;
-		UINT8  EntryPointLength;
+		SsmbiosEntry32 * p_entry32;
+		SsmbiosEntry64 * p_entry64;
 	public:
 		 CsmbiosEntry();
 		~ CsmbiosEntry();
